295994c2eab725ea58ac6592d724ed8ff3f4db34
zhangrui1 <zhangrui@power-science.com>
2014-10-11 14:29:23 +0900

    re

--- src/main/java/com/ValdacBeta/service/UserService.java
+++ src/main/java/com/ValdacBeta/service/UserService.java
@@ -1,12 +1,12 @@
 package com.ValdacBeta.service;
 
-import com.ValdacBeta.dao.UserMapper;
-import com.ValdacBeta.entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+        import com.ValdacBeta.dao.UserMapper;
+        import com.ValdacBeta.entity.User;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.stereotype.Service;
+        import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.Resource;
+        import javax.annotation.Resource;
 
 /**
  * Created by Lsr on 10/9/14.
