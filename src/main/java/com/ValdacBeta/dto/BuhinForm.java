672cda01885ef174c8bf7192a9bcda452c970f1a
lsr90518 <lb90518@gmail.com>
2014-10-18 16:17:59 +0900

    新規登録　完成

---
+++ src/main/java/com/ValdacBeta/dto/BuhinForm.java
@@ -0,0 +1,7 @@
+package com.ValdacBeta.dto;
+
+/**
+ * Created by Lsr on 10/18/14.
+ */
+public class BuhinForm {
+}
---
+++ src/main/java/com/ValdacBeta/service/BuhinService.java
@@ -0,0 +1,7 @@
+package com.ValdacBeta.service;
+
+/**
+ * Created by Lsr on 10/18/14.
+ */
+public class BuhinService {
+}
